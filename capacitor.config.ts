d35d54c2049ca03941871988fbce167a592719de
d-kurchenko <pvmnsd@gmail.com>
2025-01-08 19:50:07 +0200

    feat: rebranding

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.hymns.sda',
-  appName: 'hymns-sda',
+  appName: 'Hymns SDA',
   webDir: 'dist',
 };
 
