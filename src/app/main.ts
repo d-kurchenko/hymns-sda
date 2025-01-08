c9bfaebb59c825b1ab2d99a5a8a0e2dbbf79ad6e
d-kurchenko <pvmnsd@gmail.com>
2025-01-08 22:01:12 +0200

    fix: back gesture when routes history empty

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,10 @@
+import { App as CapacitorApp } from '@capacitor/app';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
-import App from './App.vue';
 
+import App from './App.vue';
 import router from './router';
+
 import './index.css';
 
 const app = createApp(App);
@@ -12,4 +14,11 @@ app.use(router);
 
 router.isReady().then(() => {
   app.mount('#app');
+});
+
+CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+  if (!canGoBack)
+    CapacitorApp.exitApp();
+  else
+    window.history.back();
 });
