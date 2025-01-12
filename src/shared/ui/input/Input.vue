0d880fa3f2e82d15bcdef2f2c04c00d2af134ce5
d-kurchenko <pvmnsd@gmail.com>
2025-01-12 22:47:03 +0200

    style(input): ring width, offset

--- src/shared/ui/input/Input.vue
+++ src/shared/ui/input/Input.vue
@@ -34,7 +34,7 @@ function clear() {
     <input v-bind="$attrs" v-model="modelValue"
       ref="ref"
       :class="[
-        cn('flex h-10 w-full relative rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'),
+        cn('flex h-10 w-full relative rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50'),
         {
           'pl-10': prefixIcon,
           'pr-10': clearable
