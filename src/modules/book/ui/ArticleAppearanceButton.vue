875d25a0ce55ad5547d9d8a1540bb462c67bdddd
d-kurchenko <pvmnsd@gmail.com>
2025-08-01 17:41:17 +0300

    fix(ArticleAppearanceButton): close menu on repeated button click

--- src/modules/book/ui/ArticleAppearanceButton.vue
+++ src/modules/book/ui/ArticleAppearanceButton.vue
@@ -2,19 +2,27 @@
 import FormatPaintIcon from '@material-design-icons/svg/outlined/format_paint.svg?component';
 import TextDecreaseIcon from '@material-design-icons/svg/outlined/text_decrease.svg?component';
 import TextIncreaseIcon from '@material-design-icons/svg/outlined/text_increase.svg?component';
+import { onClickOutside } from '@vueuse/core';
+import { ref, useTemplateRef } from 'vue';
 import { bookLib, bookModel } from '..';
 
 const { fontSize } = bookLib.useArticleAppearance();
+const isActive = ref(false);
+const menu = useTemplateRef('menu');
+onClickOutside(menu, () => isActive.value = false);
 </script>
 
 <template>
   <button
     class="transparent circle ripple"
+    data-ui="#menu"
   >
     <i><FormatPaintIcon /></i>
 
     <menu
       id="menu"
+      ref="menu"
+      :class="{ active: isActive }"
       class="bottom tw:top-10 no-wrap left right-align tw:overflow-visible"
     >
       <li class="tw:gap-0 hover:tw:bg-inherit tw:cursor-default">
